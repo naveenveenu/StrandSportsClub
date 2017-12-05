686914cfdb3275ce21a8f5ab4e5a837030ffac5a
Naveen Dasari <naveen@dampa.strandls.com>
2017-12-05 18:25:12 +0530

    Dec 5th commit from office system 	modified:   StrandSportsClub/public/javascripts/adminPage.js 	modified:   StrandSportsClub/public/stylesheets/main.css 	modified:   StrandSportsClub/public/views/adminPage.html 	modified:   StrandSportsClub/public/views/footer.html 	modified:   StrandSportsClub/routes/index.js

--- StrandSportsClub/public/javascripts/adminPage.js
+++ StrandSportsClub/public/javascripts/adminPage.js
@@ -11,66 +11,83 @@
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
     app.controller('adminPageCtrl', ['$scope', '$http', function($scope, $http) {
-      $scope.team = '';
-    //For ng-if
-      $scope.edit_team ='';
-      $scope.editable_index='';
-      $scope.team_book = [];
+      $scope.name='';
 
-    // to fetch from db
-    fetchTeams = function(){
-      $http.get('/team/getTeams', {}).then(function(res){
-        $scope.team_book = res.data;
-      },
-      function(err){
-        console.log(err);
-      });
-    };
-
-    fetchTeams();
-    $scope.reset = function() {
-      $scope.team ='';
-    };
-
-    $scope.submit = function(team){
-      console.log(team);
-      var teams = {};
-      teams['name'] = team;
-      $http.post('/team/addTeam', teams).then(function(res){
-          console.log(res);
-          fetchTeams();
-          $scope.reset();
-      },function(err){
-          console.log(err);
-      });
-    };
-
-    $scope.delete = function(userId){
-      // $scope.addressBook.push(address); //without backend
-      $http.post('/team/deleteTeam', {"userId" : userId}).then(function(res){
-          console.log(res);
-          fetchTeams();
-      },function(err){
-          console.log(err);
-      });
-
-      // var index = $scope.addressBook.findIndex(i => (i.name === name && i.phone === phone));
-      // console.log("index >>>>" +index);
-      // if(index > -1){
-      //   $scope.addressBook.splice(index, 1);
-      // };
-    };
-
-    $scope.saveName = function(team){
-      $http.post('/team/updateTeams', {'team': team}).then(function(res){
-        console.log(res);
-        fetchTeams();
-      },function(err){
-          console.log(err);
-      });
-      $scope.edit_team = false;
-      $scope.editableI_index = -1;
-    };
+      $scope.submit = function(name) {
+        console.log(name);
+        var sports = {};
+          sports['name'] = name;
+          console.log(sports['name']);
+          $http.post('/sport/addSport', sports).then(function(res){
+              console.log(res);
+              // fetchTeams();
+              $scope.reset();
+          },function(err){
+              console.log(err);
+          });
+      };
+    //
+    //
+    //   $scope.team = '';
+    // //For ng-if
+    //   $scope.edit_team ='';
+    //   $scope.editable_index='';
+    //   $scope.team_book = [];
+    //
+    // // to fetch from db
+    // fetchTeams = function(){
+    //   $http.get('/team/getTeams', {}).then(function(res){
+    //     $scope.team_book = res.data;
+    //   },
+    //   function(err){
+    //     console.log(err);
+    //   });
+    // };
+    //
+    // fetchTeams();
+    // $scope.reset = function() {
+    //   $scope.team ='';
+    // };
+    //
+    // $scope.submit = function(team){
+    //   console.log(team);
+    //   var teams = {};
+    //   teams['name'] = team;
+    //   $http.post('/team/addTeam', teams).then(function(res){
+    //       console.log(res);
+    //       fetchTeams();
+    //       $scope.reset();
+    //   },function(err){
+    //       console.log(err);
+    //   });
+    // };
+    //
+    // $scope.delete = function(userId){
+    //   // $scope.addressBook.push(address); //without backend
+    //   $http.post('/team/deleteTeam', {"userId" : userId}).then(function(res){
+    //       console.log(res);
+    //       fetchTeams();
+    //   },function(err){
+    //       console.log(err);
+    //   });
+    //
+    //   // var index = $scope.addressBook.findIndex(i => (i.name === name && i.phone === phone));
+    //   // console.log("index >>>>" +index);
+    //   // if(index > -1){
+    //   //   $scope.addressBook.splice(index, 1);
+    //   // };
+    // };
+    //
+    // $scope.saveName = function(team){
+    //   $http.post('/team/updateTeams', {'team': team}).then(function(res){
+    //     console.log(res);
+    //     fetchTeams();
+    //   },function(err){
+    //       console.log(err);
+    //   });
+    //   $scope.edit_team = false;
+    //   $scope.editableI_index = -1;
+    // };
 
 
   }]);
--- StrandSportsClub/routes/index.js
+++ StrandSportsClub/routes/index.js
@@ -1,16 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-var team = require('../services/teams/team');
+// var team = require('../services/teams/team');
+var sport = require('../services/sports/sport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/team/getTeams', team.getTeams);
-router.post('/team/insertTeam', team.insertTeam);
-router.post('/team/deleteTeam', team.deleteTeam);
-router.post('/team/updateTeam', team.updateTeam);
+// router.get('/team/getTeams', team.getTeams);
+// router.post('/team/insertTeam', team.insertTeam);
+// router.post('/team/deleteTeam', team.deleteTeam);
+// router.post('/team/updateTeam', team.updateTeam);
+
+router.get('/sport/getSport', sport.getSport);
+router.post('/sport/addSport', sport.insertSport);
+router.post('/sport/deleteSport', sport.deleteSport);
+router.post('/sport/updateSport', sport.updateSport);
+
 
 module.exports = router;
